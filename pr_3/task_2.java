0e4d81f8d1575547f7a132875a1721c7f4a76063
Alexandra_B <163668447+littlestudent777@users.noreply.github.com>
2024-12-05 13:55:37 +0300

    Create task_2.java

---
+++ pr_3/task_2.java
@@ -0,0 +1,5 @@
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("\u0410\u043B\u0435\u043A\u0441\u0430\u043D\u0434\u0440\u0430"); // Александра
+    }
+}
