54dedd3110f03601cf26b7faaacbe0a2383483fb
Alexandra_B <163668447+littlestudent777@users.noreply.github.com>
2024-12-05 17:23:27 +0300

    Create Task1.java

---
+++ pr_9/Task1.java
@@ -0,0 +1,15 @@
+class InfiniteWhileLoop {
+    public static void main(String[] args) {
+        while (true) {
+            // ...
+        }
+    }
+}
+
+class InfiniteDoWhileLoop {
+    public static void main(String[] args) {
+        do {
+            // ...
+        } while (true);
+    }
+}
