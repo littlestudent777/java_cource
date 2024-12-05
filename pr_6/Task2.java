a84f7d7863f625f18ef0bf15333b9b5e0a18ff79
Alexandra_B <163668447+littlestudent777@users.noreply.github.com>
2024-12-05 16:47:07 +0300

    Add Task2.java

---
+++ pr_6/Task2.java
@@ -0,0 +1,10 @@
+public class Task2 {
+    public static void main(String[] args) {
+        Boolean b1 = Boolean.valueOf(true);
+        Boolean b2 = Boolean.valueOf("true");
+        Boolean b3 = Boolean.parseBoolean("true");
+        Boolean b4 = true;
+        Boolean b5 = Boolean.TRUE;
+        // То же самое с false
+    }
+}
